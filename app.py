d2422ff19ef6be221ab02cc82927fea2f65a4eb5
Tomas Kindahl <tomas.kindahl@bahnhof.se>
2022-03-17 05:06:56 +0100

    server changes; added local mount

--- app.py
+++ app.py
@@ -16,8 +16,25 @@ def get_hit_count():
             retries -= 1
             time.sleep(0.5)
 
+def fullhtml(title, content):
+    S  = "<!DOCTYPE html>\n"
+    S += "<html>\n"
+    S += "  <head>\n"
+    S += "    <meta charset='utf-8'/>\n"
+    S += "    <title>{}</title>\n".format(title)
+    S += "  </head>\n"
+    S += "  <body>\n"
+    S += "    <h1>{}</h1>\n".format(title)
+    S += "    {}\n".format(content)
+    S += "  </body>\n"
+    S += "</html>\n"
+    return S
+
 @app.route('/')
 def hello():
     count = get_hit_count()
-    return 'Hello World! I have been seen {} times.\n'.format(count)
+    return 'Guten Tag Welt! Ich bin {} Weile gesehen.\n'.format(count)
 
+@app.route('/ykk')
+def ykk():
+    return fullhtml("Ykk!","<p>Ykk!</p>")
